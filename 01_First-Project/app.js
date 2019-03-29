c8c5ac35cea21ab2703a6468319efba6a9deb5dd
samuel-puni <samuel.puni@gmail.com>
2019-03-29 18:31:44 -0400

    Continuacion del juego

--- 01_First-Project/app.js
+++ 01_First-Project/app.js
@@ -3,44 +3,75 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns: []
     },
     methods: {
         startGame: function() {
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns = [];
         },
         attack: function() {
-            this.monsterHealth -= this.calculateDamage(3, 10);
+            var damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for ' + damage
+            })
             if(this.checkWin()) {
                 return;
             }
 
-            this.playerHealth -= this.calculateDamage(5, 12);
-            this.checkWin();
+            this.monsterAttacks();
         },
         specialAttack: function() {
-
+            var damage = this.calculateDamage(10, 20);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster hard for ' + damage
+            })
+            if(this.checkWin()) {
+                return;
+            }
+            this.monsterAttacks();
         },
         heal: function() {
-
+            if (this.playerHealth <= 90) {
+                this.playerHealth += 10;
+            } else {
+                this.playerHealth = 100;
+            }
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player heals for 10'
+            })
+            this.monsterAttacks();
         },
         giveUp: function() {
-
+            this.gameIsRunning = false;
         },
         calculateDamage: function(min, max) {
             return Math.max(Math.floor(Math.random() * max) + 1, min);
         },
         monsterAttacks: function () {
-            
-        }
+            var damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
+            this.checkWin();
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits Player for ' + damage
+            })          
+        },
         checkWin: function () {
             if (this.monsterHealth <= 0) {
                 if (confirm('Ganaste!!! Otro juego?')) {
                     this.startGame();
                 } else {
                     this.gameIsRunning = false;
+                    this.turns = [];
                 }
                 return true;
             } else if (this.playerHealth <= 0) {
@@ -48,6 +79,7 @@ new Vue({
                     this.startGame();
                 } else {
                     this.gameIsRunning = false;
+                    this.turns = [];
                 }
                 return true;
             }
